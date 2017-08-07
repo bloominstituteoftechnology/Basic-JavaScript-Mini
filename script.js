7396cf55c5c1e0aeccf3746cf822192dac0d540c
sunjieming <ben@lambdaschool.com>
2017-08-07 09:20:04 -0700

    Added README and script

---
+++ script.js
@@ -0,0 +1,22 @@
+var logger = function() {
+	var message = 'hi';
+	console.log(message);
+};
+
+var looper = function() {
+	var message = 'I love JS!';
+	for (var i = 0; i < 10; i++) {
+		console.log(message);
+	}
+};
+
+var greet = function(name) {
+	var greeting = 'Hello ' + name + '!';
+	console.log(greeting);
+};
+
+var yourName = 'put your name in here';
+
+logger();
+looper();
+greet(yourName);
