d89466a677fe4c3a20c0030b7a6189777c863081
Camucamu <drewmueller4@gmail.com>
2017-08-07 11:33:52 -0700

    changed to es6

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
-var logger = function() {
-	var message = 'hi';
+const logger = () => {
+	const message = 'hi';
 	console.log(message);
 };
 
-var looper = function() {
-	var message = 'I love JS!';
-	for (var i = 0; i < 10; i++) {
+const looper = () => {
+	const message = 'I love JS!';
+	for (let i = 0; i < 10; i++) {
 		console.log(message);
 	}
 };
 
-var greet = function(name) {
-	var greeting = 'Hello ' + name + '!';
+const greet = (name) => {
+	const greeting = (`Hello ${name}!`);
 	console.log(greeting);
 };
 
-var yourName = 'put your name in here';
+const yourName = 'Drew';
 
 logger();
 looper();
