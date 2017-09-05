ef14dcc81ba26adc56fe75e33ddf15e8459507bc
Collin Ferguson <theredcouchroom@gmail.com>
2017-09-05 14:49:18 -0500

    Changed to CS6

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const logger = () => {
 
 const looper = () => {
   const message = 'I love JS!';
-  for (var i = 0; i < 10; i++) {
+  for (let i = 0; i < 10; i++) {
     console.log(message);
   }
 };
