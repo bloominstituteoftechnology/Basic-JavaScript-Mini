c4c030861a9ac3a466618d94c8cb01488f691f83
John Pelley <jryanp@yahoo.com>
2017-09-05 15:57:04 -0700

    fixed script I hope

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-const logger = ()=> {
+const logger = () => {
   const message = 'hi';
   console.log(message);
 };
 
 const looper = () => {
-  } var message = 'I love JS!';
+  const message = 'I love JS!';
   for (let i = 0; i < 10; i++) {
     console.log(message);
   }
 };
 
 const greet = (name)=> {
-  var greeting = 'Hello my name is ${name}!';
+  const greeting = `Hello my name is ${name}!`;
 
   console.log(greeting);
 };
