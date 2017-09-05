667d06fe1fba53389aa2f7407f916550115a7364
Mathew Calkins <mathewpcalkins@gmail.com>
2017-09-05 15:39:48 -0400

    Changed code to ES6

--- script.js
+++ script.js
@@ -1,21 +1,55 @@
+/*
 var logger = function() {
   var message = 'hi';
   console.log(message);
 };
+*/
+const logger = () => {
+  let message = 'hi';
+  console.log(message);
+}
 
+
+
+
+
+/*
 var looper = function() {
   var message = 'I love JS!';
-  for (var i = 0; i < 10; i++) {
+  // for (var i = 0; i < 10; i++) {
+  for (let i = 0; i < 10; i++) {
     console.log(message);
   }
 };
+*/
+const looper = () => {
+  let message = 'I love JS!';
+  for (let i = 0; i < 10; i++) {
+    console.log(message);
+  }
+}
 
+
+
+
+
+/*
 var greet = function(name) {
   var greeting = 'Hello ' + name + '!';
   console.log(greeting);
 };
+*/
+const greet = (name) => {
+  let greeting = 'Hello ${name}!';
+  console.log(greeting);
+}
 
-var yourName = 'put your name in here';
+
+
+
+
+//var yourName = 'put your name in here';
+var yourName = 'Mathew"
 
 logger();
 looper();
