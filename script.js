04839df13ffc4b2578204b11e7862731858a5c30
Samer Dabra <samer.dabra@googlemail.com>
2017-09-05 21:24:23 +0100

    Changed code to ES6 - quick edit (let to const)

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const logger = () => console.log(`hi`);
 
 const looper = () => {
-	let message = 'I love JS!';
-	for (let i = 0; i < 10; i++) {
-		console.log(message);
-	}
+ const message = 'I love JS!';
+  for (let i = 0; i < 10; i++) {
+    console.log(message);
+  }
 };
 
 const greet = (name) => console.log(`Hello ${name}!`);
