4a5e56984c9824bafd9128870b6465ecbdf4840d
Adam Oates <writingyourcareer@gmail.com>
2017-09-06 23:09:50 +0000

    completed basic javascript mini by Adam Oates

--- script.js
+++ script.js
@@ -1,21 +1,17 @@
-var logger = function() {
-  var message = 'hi';
-  console.log(message);
+const logger = () => {
+  console.log(`hi`);
 };
 
-var looper = function() {
-  var message = 'I love JS!';
-  for (var i = 0; i < 10; i++) {
+const looper = () => {
+  const message = `I love JS!`;
+  for (let i = 0; i < 10; i++) {
     console.log(message);
   }
 };
 
-var greet = function(name) {
-  var greeting = 'Hello ' + name + '!';
-  console.log(greeting);
-};
-
-var yourName = 'put your name in here';
+const greet = (name) => console.log(`Hello  + name ${name}!`);
+  
+const yourName = 'Adam Oates';
 
 logger();
 looper();
